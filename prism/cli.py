b3af61565e218c5443e116c109053a5e977b5407
CurtLH <CurtLHampton@gmail.com>
2021-01-08 23:56:59 +0000

    Change dataset references to table references

--- prism/cli.py
+++ prism/cli.py
@@ -34,7 +34,7 @@ def main(ctx, base_url, tenant_name, client_id, client_secret, refresh_token):
     """CLI for interacting with Workday’s Prism API"""
 
     # initialize the prism class with your credentials
-    p = prism.Prism(base_url, tenant_name, client_id, client_secret, refresh_token)
+    p = prism.Prism(base_url, tenant_name, client_id, client_secret, refresh_token, version="v2")
 
     # create the bearer token
     p.create_bearer_token()
@@ -44,20 +44,20 @@ def main(ctx, base_url, tenant_name, client_id, client_secret, refresh_token):
 
 
 @main.command()
-@click.option("--id", default=None, type=str, help="The ID of the dataset to obtain details about")
+@click.option("--name", default=None, type=str, help="The name of the table to obtain details about")
 @click.pass_context
-def list(ctx, id):
-    """List all datasets of type API"""
+def list(ctx, name):
+    """List all tables of type API"""
 
     # get the initialized prism class
     p = ctx.obj["p"]
 
-    # list the datasets
-    status = p.list_dataset(dataset_id=id)
+    # list the tables
+    status = p.list_table(table_name=name)
 
     # print message
     if id is None:
-        click.echo("There are {} API datasets".format(status["total"]))
+        click.echo("There are {} API tables".format(status["total"]))
         click.echo(json.dumps(status["data"], indent=2, sort_keys=True))
     else:
         click.echo(json.dumps(status, indent=2, sort_keys=True))
@@ -65,31 +65,31 @@ def list(ctx, id):
 
 @main.command()
 @click.option(
-    "--dataset_name",
+    "--table_name",
     type=str,
     required=True,
-    help="The dataset name. The name must be unique and conform to the name validation rules",
+    help="The table name. The name must be unique and conform to the name validation rules",
 )
 @click.option("--schema_path", type=click.Path(), required=True, help="The path to your schema file")
 @click.option("--data_path", type=click.Path(), required=True, help="The path to your gzip compressed data file")
 @click.pass_context
-def upload(ctx, dataset_name, schema_path, data_path):
+def upload(ctx, table_name, schema_path, data_path):
     """Upload a gzip CSV file"""
 
     # get the initialized prism class
     p = ctx.obj["p"]
 
-    # clean up the dataset name
-    dataset_name = dataset_name.replace(" ", "_")
+    # clean up the table name
+    table_name = table_name.replace(" ", "_")
 
-    # create an empty API dataset
-    dataset = p.create_dataset(dataset_name)
+    # create an empty API table
+    table = p.create_table(table_name)
 
-    # read in your dataset schema
+    # read in your table schema
     schema = prism.load_schema(schema_path)
 
     # create a new bucket to hold your file
-    bucket = p.create_bucket(schema, dataset["id"])
+    bucket = p.create_bucket(schema, table["id"])
 
     # add your file the bucket you just created
     p.upload_file_to_bucket(bucket["id"], data_path)
@@ -97,11 +97,11 @@ def upload(ctx, dataset_name, schema_path, data_path):
     # complete the bucket and upload your file
     p.complete_bucket(bucket["id"])
 
-    # check the status of the dataset you just created
-    status = p.list_dataset(dataset["id"])
+    # check the status of the table you just created
+    status = p.list_table(table["id"])
 
     # print message
-    click.echo("{} has successfully uploaded".format(dataset_name))
+    click.echo("{} has successfully uploaded".format(table_name))
     click.echo(json.dumps(status["data"], indent=2, sort_keys=True))
 
 
